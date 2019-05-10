59d4b16b52f93da3f89034c99515ba04961c158e
Stefan Scepanovic <scepa91@icloud.com>
2019-05-10 16:27:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -49,9 +49,14 @@ def adding
 shipping_manifest["gun powder"] = "4"
 
 
-"whale bone corsets" => 5
-"porcelain vases" => 2
-"oil paintings" => 3
+shipping_manifest = {
+                "whale bone corsets" => 5,
+                "porcelain vases" => 2,
+                "oil paintings" => 3,
+                "muskets" => 2,
+                "gun powder" => 4
+               }
+
 "muskets" => 2
 "gun powder" => 4
 
